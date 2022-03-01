125e8e358b1930fb4c1882184510c87a21c19e56
Jayanth Kumar K H <jayanthkumar.kh@utthunga.in>
2022-03-01 17:00:17 +0530

    area file

---
+++ prac.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int r;
+    float pi = 3.14;
+    printf("enter the radius of the circle:");
+    scanf("%d", &r);
+
+    float area = (pi * r * r);
+
+    printf("the area of the circle is : %f", area);
+
+    return 0;
+}
+
+#include <stdio.h>
+int main()
+{
+    int h, b;
+    printf("enter the hieght and base  of the triangle:");
+    scanf("%d %d", &b, &h);
+
+    int area = (h * b) / 2;
+
+    printf("the area of the circle is : %d", area);
+
+    return 0;
+}
